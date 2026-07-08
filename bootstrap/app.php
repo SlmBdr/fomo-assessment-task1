42d582ecc1c6fa75eee71d62df889a16ec5cfeac
SlmBdr <salambadar1@gmail.com>
2026-07-08 10:06:19 +0700

    Fix vercel routing to execute PHP function

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,40 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+if (env('VERCEL')) {
+    $app = Application::configure(basePath: dirname(__DIR__))
+        ->withRouting(
+            web: __DIR__.'/../routes/web.php',
+            commands: __DIR__.'/../routes/console.php',
+            health: '/up',
+        )
+        ->withMiddleware(function (Middleware $middleware) {
+            $middleware->validateCsrfTokens(except: [
+                '*'
+            ]);
+        })
+        ->withExceptions(function (Exceptions $exceptions) {
+            //
+        })->create();
+
+    $app->useStoragePath('/tmp/storage');
+
+    if (!is_dir('/tmp/storage/framework/views')) {
+        mkdir('/tmp/storage/framework/views', 0755, true);
+    }
+    if (!is_dir('/tmp/storage/framework/cache/data')) {
+        mkdir('/tmp/storage/framework/cache/data', 0755, true);
+    }
+    if (!is_dir('/tmp/storage/framework/sessions')) {
+        mkdir('/tmp/storage/framework/sessions', 0755, true);
+    }
+    if (!is_dir('/tmp/storage/logs')) {
+        mkdir('/tmp/storage/logs', 0755, true);
+    }
+
+    return $app;
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,7 +45,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Disable CSRF for our API endpoints for simplicity
         $middleware->validateCsrfTokens(except: [
             '*'
         ]);
